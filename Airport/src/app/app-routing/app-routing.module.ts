bfe9f43473b5acb261e865fa28d1d337c118474f
Anastasiia <nastena31011993@gmail.com>
2018-07-25 21:43:18 +0300

    Added routing for pilot

--- Airport/src/app/app-routing/app-routing.module.ts
+++ Airport/src/app/app-routing/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { PilotListComponent } from '../controllers-module/pilot-list/pilot-list.component';
+import { PilotDetailComponent } from '../controllers-module/pilot-detail/pilot-detail.component';
+import { HomeComponent } from '../controllers-module/home/home.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'pilots', component: PilotListComponent },
+  { path: 'pilots/:id', component: PilotDetailComponent },
+  { path: 'home', component: HomeComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- Airport/src/app/app.module.ts
+++ Airport/src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { PilotListComponent } from './controllers-module/pilot-list/pilot-list.component';
+import { PilotDetailComponent } from './controllers-module/pilot-detail/pilot-detail.component';
+import { HomeComponent } from './controllers-module/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PilotListComponent,
+    PilotDetailComponent
   ],
   imports: [
     BrowserModule,
