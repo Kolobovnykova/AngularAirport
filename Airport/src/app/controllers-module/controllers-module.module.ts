24d5b492a18b0158e1885f9369d4e98f6b4b3319
Anastasiia <nastena31011993@gmail.com>
2018-07-25 21:03:42 +0300

    Added app-routing module and home component

--- Airport/src/app/controllers-module/controllers-module.module.ts
+++ Airport/src/app/controllers-module/controllers-module.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PilotListComponent } from './pilot-list/pilot-list.component';
 import { PilotDetailComponent } from './pilot-detail/pilot-detail.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [PilotListComponent, PilotDetailComponent]
+  declarations: [PilotListComponent, PilotDetailComponent, HomeComponent]
 })
 export class ControllersModuleModule { }
