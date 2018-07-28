a822199090f487fb389ce484e4b20a925a3b2a0c
Anastasiia <nastena31011993@gmail.com>
2018-07-28 04:05:16 +0300

    Added planetype list

--- Airport/src/app/components/planetype-list/planetype-list.component.ts
+++ Airport/src/app/components/planetype-list/planetype-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { PlanetypeService } from '../../services/planetype.service';
+import { PlaneType } from '../../services/Models/planetype';
 
 @Component({
   selector: 'app-planetype-list',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanetypeListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private planeTypeService: PlanetypeService,
+    private location: Location
+  ) { }
+  private router: Router
+  planetypes: PlaneType[];
 
   ngOnInit() {
+    this.getAll();
   }
 
+  getAll(): void {
+    this.planeTypeService.getAll()
+      .subscribe(planetypes => this.planetypes = planetypes);
+  }
+
+  delete(planetype: PlaneType): void {
+    const idToDelete = planetype.id;
+    this.planeTypeService.delete(idToDelete)
+      .subscribe((planetypeToDelete) => this.planetypes = this.planetypes.filter(
+        (pilot) => pilot.id !== idToDelete));
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
